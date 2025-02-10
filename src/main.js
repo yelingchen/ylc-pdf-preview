4595d233317cd5bbf84e0a2a7f77101c7b565963
yelingchen <83534873@qq.com>
2025-02-10 09:13:04 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import * as PDFJS from 'pdfjs-dist';
-PDFJS.GlobalWorkerOptions.workerSrc = '../public/pdf.worker.min.js';
+PDFJS.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
 
 const app = createApp(App)
 
